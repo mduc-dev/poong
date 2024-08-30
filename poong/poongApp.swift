245d48b48c2b7648f3bd64f8c28a1cfc3f87dced
mduc-dev <duc040301@gmail.com>
2024-08-30 14:10:33 +0700

    initial project

---
+++ poong/poongApp.swift
@@ -0,0 +1,17 @@
+//
+//  poongApp.swift
+//  poong
+//
+//  Created by Duc Nguyen on 30/8/24.
+//
+
+import SwiftUI
+
+@main
+struct poongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
